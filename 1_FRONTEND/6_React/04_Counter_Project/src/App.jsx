a197bfb1974b7a2b8264563113fd0997769b1a93
Harsh Kumar Mahto <harshkumaraunak@gmail.com>
2024-09-06 12:35:13 +0530

    Hooks

---
+++ 1_FRONTEND/6_React/04_Counter_Project/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)  //useState is a hook in which we give to things, first one is variable and another one is the function that is used to update the value of the variable
+
+  return (
+    <>
+      <h1>Counter Project</h1>
+      Count is: {count}
+      <br />
+      <button onClick={()=>{if(count<20)setCount(count=>count+1)}}>Increase</button>    
+      <button onClick={()=>{if(count>0)setCount(count=>count-1)}}>Decrease</button>
+    </>
+  )
+}
+
+export default App

16db5f29f723d0ee5ecef51aa41ce554791d8785
Harsh Kumar Mahto <harshkumaraunak@gmail.com>
2024-09-06 12:55:33 +0530

    Virtual DOM

--- 1_FRONTEND/6_React/04_Counter_Project/src/App.jsx
+++ 1_FRONTEND/6_React/04_Counter_Project/src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)  //useState is a hook in which we give to things, first one is variable and another one is the function that is used to update the value of the variable
+  //This hook is used to propogate the changed values to the UI of the website
 
   return (
     <>
